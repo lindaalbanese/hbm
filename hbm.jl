f044e53479b99df78eb30ca4813bdc309e241e56
lindaalbanese <70444225+lindaalbanese@users.noreply.github.com>
2020-08-30 23:25:31 +0200

    conditional probabilities and hamiltonian

--- hbm.jl
+++ hbm.jl
@@ -2,8 +2,19 @@ using PyPlot
 
 function mctransition(β,ξ,σ,z) #prende uno stato del sistema in ingresso e tira fuori un nuovo stato
     σn=copy(σ)
+    N=length(σn)
     zn=copy(z)
+    P=lenght(zn)
+    ξn=copy(z)
+    fact=zn*ξn
+    prob_si=empty(N)
+    for i = 1:N
+        prob_si[i]= exp(β*σn[i]*fact[i])/(exp(β*fact[i]) + exp(-β*fact[i]))
+    prob_s = prod(prob_si) #P(σn|zn,ξn,β) 
+    avg= ξn*transpose(σn) 
+    prob_z = (β/(2*π))^(P/2)*exp(-β/2*sum((zn[mu] - avg[mu])^2) for mu =1:P) #P(zn|σn,ξn,β)
     #aggiungere gibbs update per σ e z, compiti: calcolare P(σ|z,ξ,β) e P(z|σ,ξ,β)
+    ham = 1/2*sum(zn^2) - sum(σn*transpose(fact)) #H(σ, z, ξ) 
     (σn,zn)
 end
 
