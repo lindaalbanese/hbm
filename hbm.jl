62103ed6092b5cd4143f525a2f0b6be8c16a3ad0
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-08-30 18:10:23 +0200

    MCMC structure

--- hbm.jl
+++ hbm.jl
@@ -1,6 +1,18 @@
 using PyPlot
-pygui(true)
-plot(1:10)
-print("hello")
 
-#test
+function mctransition(β,ξ,σ,z) #prende uno stato del sistema in ingresso e tira fuori un nuovo stato
+    σn=copy(σ)
+    zn=copy(z)
+    #aggiungere gibbs update per σ e z, compiti: calcolare P(σ|z,ξ,β) e P(z|σ,ξ,β)
+    (σn,zn)
+end
+
+function mcmc_chain(β,ξ,σ,z,N) #prende uno stato in ingresso e tira fuori una catena di stati
+    chain=fill((σ,z),N)
+    chain[1] = mctransition(β,ξ,σ,z)
+    for i = 2:N
+        chain[i] = mctransition(β,ξ,chain[i-1]...)
+    end
+    chain
+end
+
